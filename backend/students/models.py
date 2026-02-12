f174dedf755361ffe9246b43e01db587869c9024
Asad M <asadmm369@gmail.com>
2026-02-12 15:55:05 -0500

    Working on the student models

--- backend/students/models.py
+++ backend/students/models.py
@@ -10,6 +10,22 @@ class Student(models.Model):
     name = models.CharField(max_length=255)
     date_of_birth = models.DateField()
     enrollment_date = models.DateField()
+
+
+
+class StudentSchedule(models.Model):
+    student = models.ForeignKey(
+        Student,
+        on_delete=models.CASCADE,
+        related_name="student_schudule"
+    )
+    start_time = models.TimeField()
+    end_time = models.TimeField()
+
+
+class DaysOfWeek(models.CharField):
+    monday = "monday, Monday"
+    
     
     
     
